19e68a2fdc110703d652b2a09e1e9f66bd0d6218
yuzhao88 <yz473@drexel.edu>
2015-02-18 21:29:48 -0500

    Add query to second question: given a question is answered, show next question with the highest answer rate

--- query.sql
+++ query.sql
@@ -14,13 +14,36 @@ group by sId
 from person
 where sEvent = "ANS"
 group by sId)  ans  on (total.sId = ans.sId)
-order by rate desc
+order by rate desc;
 
 
 # Given a person has answer question A, find the question next question to show. 
 # specifically show next question with highest answer rate given question a has been answered
 
+select pid
+from person
+where sEvent = "ANS" and sId = 1;
 
 
-
-
+select tot.sId, ans.num / tot.num
+from
+(select sid, count(sEvent) as num
+from person
+where pId in 
+(
+select pId
+from person
+where sEvent = "ANS" and sId = 1
+) and sEvent = "IMP"
+group by sid
+) tot left outer join 
+(select sid, count(sEvent) as num
+from person
+where pId in 
+(
+select pId
+from person
+where sEvent = "ANS" and sId = 1
+) and sEvent = "ANS"
+group by sid
+) ans  on (tot.sId = ans.sId)
